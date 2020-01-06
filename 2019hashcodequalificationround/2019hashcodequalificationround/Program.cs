5408af008cb9fd862852730c81a9fca1684cd4a7
Komal Shehzadi <shehzadikomal303@gmail.com>
2020-01-07 01:41:22 +0500

    Slides Created with replicated tags

--- 2019hashcodequalificationround/2019hashcodequalificationround/Program.cs
+++ 2019hashcodequalificationround/2019hashcodequalificationround/Program.cs
@@ -9,10 +9,24 @@ namespace _2019hashcodequalificationround
     {
         static void Main(string[] args)
         {
-            string text = File.ReadAllText("C:\\Users\\DC\\source\\repos\\2019hashcodequalificationround\\qualification_round_2019.in\\a_example.txt");
+            string text = File.ReadAllText("C:\\Users\\DC\\source\\repos\\2019hashcodequalificationround\\qualification_round_2019.in\\c_memorable_moments.txt");
             Console.WriteLine(text);
             Console.WriteLine("****************File Read Successful!!*****************");
+            Console.WriteLine("*********************Slides Created*********************");
             List<Utility.pictureinfo> mypictures = Utility.GetPictureList(text);
+            List<Utility.slides> myslides = Utility.GetSlidesFromPictures(mypictures);
+            Console.WriteLine(myslides.Count);
+            foreach(Utility.slides s in myslides)
+            {
+                if(s.numberofpictures == 1)
+                {
+                    Console.WriteLine(s.pictureid[0]);
+                }
+                else
+                {
+                    Console.WriteLine(s.pictureid[0] + " " + s.pictureid[1]);
+                }
+            }
             Console.WriteLine("Break");
         }
         
--- 2019hashcodequalificationround/2019hashcodequalificationround/Utility.cs
+++ 2019hashcodequalificationround/2019hashcodequalificationround/Utility.cs
@@ -12,7 +12,51 @@ namespace _2019hashcodequalificationround
             return lines.Length >= lineNo ? lines[lineNo - 1] : null;
         }
         
-        
+        public static List<slides> GetSlidesFromPictures(List<pictureinfo> pictures)
+        {
+            List<slides> slideslist = new List<slides>();
+            int i = 1;
+            int flag = 0;
+            slides v = new slides();
+            v.numberofpictures = 2;
+            foreach (pictureinfo p in pictures)
+            {
+               
+                if(p.Rotation == "H")
+                {
+                    slides s = new slides();
+                    s.id = i;
+                    s.numberofpictures = 1;
+                    s.pictureid.Add(p.id.ToString());
+                    foreach(string tag in p.tags)
+                    {
+                        s.slidetags.Add(tag);
+                    }
+                    slideslist.Add(s);
+                    i++;
+                }
+                else if(p.Rotation=="V")
+                {
+                
+                    v.pictureid.Add(p.id.ToString());
+                    foreach (string tag in p.tags)
+                    {
+                        v.slidetags.Add(tag);
+                    }
+                    flag++;
+                    if (flag == 2)
+                    {
+                        v.id = i;
+                        i++;
+                        flag = 0;
+                        slideslist.Add(v);
+                        v = new slides();
+                        v.numberofpictures = 2;
+                    }
+                }
+            }
+            return slideslist;
+        }
         public static List<pictureinfo> GetPictureList(string text)
         {
             int numberofpictures = Convert.ToInt32(Utility.GetLine(text, 1));
@@ -45,7 +89,10 @@ namespace _2019hashcodequalificationround
 
         public class slides
         {
-
+            public int id;
+            public int numberofpictures;
+            public List<string> pictureid = new List<string>();
+            public List<string> slidetags = new List<string>();
         }
     }
 }
