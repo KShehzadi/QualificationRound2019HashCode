56baf8528ae7646539e0c04716fbb728eb11cf61
Komal Shehzadi <shehzadikomal303@gmail.com>
2020-01-06 21:49:44 +0500

    Empty Project

---
+++ 2019hashcodequalificationround/2019hashcodequalificationround/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace _2019hashcodequalificationround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("C:\\Users\\DC\\source\\repos\\2019hashcodequalificationround\\qualification_round_2019.in\\a_example.txt");
+            Console.WriteLine(text);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
