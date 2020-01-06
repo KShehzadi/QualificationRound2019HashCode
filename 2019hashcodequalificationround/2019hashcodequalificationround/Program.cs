6f9073f49723b8fd5ee6545cd8beb880b1384a39
Komal Shehzadi <shehzadikomal303@gmail.com>
2020-01-07 00:42:35 +0500

    Picture Info DS and fill it

--- 2019hashcodequalificationround/2019hashcodequalificationround/Program.cs
+++ 2019hashcodequalificationround/2019hashcodequalificationround/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Text;
 
 namespace _2019hashcodequalificationround
 {
@@ -9,7 +11,11 @@ namespace _2019hashcodequalificationround
         {
             string text = File.ReadAllText("C:\\Users\\DC\\source\\repos\\2019hashcodequalificationround\\qualification_round_2019.in\\a_example.txt");
             Console.WriteLine(text);
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("****************File Read Successful!!*****************");
+            List<Utility.pictureinfo> mypictures = Utility.GetPictureList(text);
+            Console.WriteLine("Break");
         }
+        
     }
+    
 }
---
+++ 2019hashcodequalificationround/2019hashcodequalificationround/Utility.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2019hashcodequalificationround
+{
+    class Utility
+    {
+        public static string GetLine(string text, int lineNo)
+        {
+            string[] lines = text.Replace("\r", "").Split('\n');
+            return lines.Length >= lineNo ? lines[lineNo - 1] : null;
+        }
+        
+        
+        public static List<pictureinfo> GetPictureList(string text)
+        {
+            int numberofpictures = Convert.ToInt32(Utility.GetLine(text, 1));
+            List<Utility.pictureinfo> picturelist = new List<Utility.pictureinfo>();
+            for (int i = 1; i <= numberofpictures; i++)
+            {
+                Utility.pictureinfo p = new Utility.pictureinfo();
+                p.id = i - 1;
+                string line = Utility.GetLine(text, i + 1);
+                string[] linesplit = line.Split(" ");
+                p.Rotation = linesplit[0];
+                p.numberoftags = Convert.ToInt32(linesplit[1]);
+                List<string> sampletags = new List<string>();
+                for (int j = 0; j < p.numberoftags; j++)
+                {
+                    sampletags.Add(linesplit[j + 2]);
+                }
+                p.tags = sampletags;
+                picturelist.Add(p);
+            }
+            return picturelist;
+        }
+        public class pictureinfo
+        {
+            public int id { get; set; }
+            public string Rotation { get; set; }
+            public int numberoftags { get; set; }
+            public List<string> tags { get; set; }
+        }
+
+        public class slides
+        {
+
+        }
+    }
+}
